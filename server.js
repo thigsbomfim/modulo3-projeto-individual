9db67317407ae6ed140f725aa7ddb35bf3ab4bc3
thigsbomfim <113481183+thigsbomfim@users.noreply.github.com>
2022-11-20 18:08:22 -0300

    Update server.js

--- server.js
+++ server.js
@@ -2,9 +2,18 @@ import  jsonServer  from 'json-server'
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
-const port = process.env.PORT || 3200;
 
 server.use(middlewares);
 server.use(router)
 
-server.listen(port)
+server.use(jsonServer.rewriter('./routes.json'))
+
+server.use((req, res,) => {
+  if (req.method === 'POST') {
+    req.createdAt = server.use(jsonServer.rewriter({"/clientes/create": `/clientes/${req.id}`}))
+  }
+})
+
+server.listen(3000, () => {
+  console.log('JSON Server is running')
+})
