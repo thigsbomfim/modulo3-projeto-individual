d183302e2ae7ecee7b47d303ba915640aaf87b97
thigsbomfim <113481183+thigsbomfim@users.noreply.github.com>
2022-11-20 21:24:02 -0300

    Update server.js

--- server.js
+++ server.js
@@ -6,16 +6,9 @@ const middlewares = jsonServer.defaults()
 server.use(middlewares);
 server.use(router)
 
-server.use((req, res,) => {
-  if (req.method === 'POST') {
-    req.createdAt = server.use(jsonServer.rewriter({"/clientes/create": `/clientes/${req.id}`}))
-  }
-})
+server.use(jsonServer.rewriter('./routes.json'))
 
 server.listen(3000, () => {
   console.log('JSON Server is running')
 })
 
-fetch('http://localhost:3000/')
-  .then((res)=> res.json())
-  .then((resultado) => resultado.use(jsonServer.rewriter('./routes.json')))
