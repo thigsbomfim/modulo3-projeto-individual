e8ec540d1f2ab906cb8e89969ac8e07012ce65d1
thigsbomfim <113481183+thigsbomfim@users.noreply.github.com>
2022-11-20 21:47:34 -0300

    Update server.js

--- server.js
+++ server.js
@@ -4,9 +4,8 @@ const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
 
 server.use(middlewares);
-server.use(router)
-
 server.use(jsonServer.rewriter('./routes.json'))
+server.use(router)
 
 server.listen(3000, () => {
   console.log('JSON Server is running')
