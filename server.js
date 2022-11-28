ed0082a9b53561174561be5f28ffd87a8a21d42f
thigsbomfim <113481183+thigsbomfim@users.noreply.github.com>
2022-11-27 22:21:28 -0300

    Update server.js

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const middlewares = jsonServer.defaults()
 
 server.use((req, res, next)=>{
   res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE")
   server.use(cors())
   next()
 })
