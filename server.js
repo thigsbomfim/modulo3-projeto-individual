d467a3ce19e67d9e802d527ffbc3a4c79aa96279
thigsbomfim <113481183+thigsbomfim@users.noreply.github.com>
2022-11-29 19:45:30 -0300

    Update server.js

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ server.use(middlewares);
 
 server.use((req, res, next)=> {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', ['GET','POST','PUT','DELETE']);
+  res.header('Access-Control-Allow-Credentials', true);
   next()
 })
 
