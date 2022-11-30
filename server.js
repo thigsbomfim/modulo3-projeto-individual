a3e350b1c756e364d01d494d7807df2ff9e541e0
thigsbomfim <113481183+thigsbomfim@users.noreply.github.com>
2022-11-30 00:34:05 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,14 @@
 const jsonServer = require('json-server');
-const cors = require('cors')
 
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
-
+const cors = require('cors')
 server.use(middlewares);
 server.use(cors())
-
 server.use((req, res, next)=> {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', ['GET','POST','PUT','DELETE']);
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE']);
   res.header('Access-Control-Allow-Credentials', true);
   res.header('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization');
   next()
