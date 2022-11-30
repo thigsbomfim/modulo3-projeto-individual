fd412c86eefd60041820274404549311a4755226
thigsbomfim <113481183+thigsbomfim@users.noreply.github.com>
2022-11-30 00:37:25 -0300

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ server.use(middlewares);
 server.use(cors())
 server.use((req, res, next)=> {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE']);
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.header('Access-Control-Allow-Credentials', true);
   res.header('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization');
   next()
