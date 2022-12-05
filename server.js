285b9c1c0f1027afed9d6a6d9e6fe4e08feb4a1b
thigsbomfim <113481183+thigsbomfim@users.noreply.github.com>
2022-12-05 02:43:16 -0300

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 const cors = require('cors')
+
 server.use(middlewares);
+
 server.use((req, res, next)=> {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
@@ -12,7 +14,9 @@ server.use((req, res, next)=> {
   res.header('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization');
   next()
 })
+
 server.use(cors())
+
 server.use(jsonServer.rewriter({
   "/produtos/wheyprotein": "/produtos?tipo_like=whey",
     "/produtos/creatina": "/produtos?tipo_like=creatina",
